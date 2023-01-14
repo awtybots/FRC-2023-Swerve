ee0f9737962f43388d1d01c4ff89a07df28c780c
Terence Dumas <dumast@awty.org>
2023-01-14 12:11:17 -0600

    dummy forward code

--- src/main/java/frc/robot/autos/forward.java
+++ src/main/java/frc/robot/autos/forward.java
@@ -31,9 +31,9 @@ public class forward extends SequentialCommandGroup {
                 // Start at the origin facing the +X direction
                 new Pose2d(0, 0, new Rotation2d(0)),
                 // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                List.of(new Translation2d(0, 1)),
                 // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, new Rotation2d(0)),
+                new Pose2d(0, 1, new Rotation2d(0)),
                 config);
 
         var thetaController =
