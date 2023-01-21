fa1915f752fde38cba3da38e321ba3d11a80a73b
Terence Dumas <dumast@awty.org>
2023-01-21 15:08:15 -0600

    pathweaver test

---
+++ src/main/java/frc/robot/autos/PathWeaverTest.java
@@ -0,0 +1,8 @@
+package frc.robot.autos;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+
+public class PathWeaverTest {
+    String trajectoryJSON = "/YourPath.wpilib.json";
+    Trajectory trajectory = new Trajectory();
+}
