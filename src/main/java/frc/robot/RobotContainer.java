95f3449d7c1ebcce82ead90f0c01c13aa9c7be64
Mateo Cirino <24cirinom@awty.org>
2023-02-26 15:04:03 -0600

    pathplanner changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,13 +92,13 @@ public class RobotContainer {
      * event markers can be created in PathPlanner.
      */
     private void eventAssignemnt() {
-        eventMap.put("event", new StowPosition(s_Elevator, s_Arm, s_Claw));
-        eventMap.put("PickUp", new IntakeFromGroundPosition(s_Elevator, s_Arm, s_Claw));
-        eventMap.put("PickupStow", new StowPosition(s_Elevator, s_Arm, s_Claw));
-        eventMap.put("stopEvent", new Balance(s_Swerve));
-        eventMap.put("Place", new MidNodePosition(s_Elevator, s_Arm, s_Claw));
-        eventMap.put("PlaceStow", new StowPosition(s_Elevator, s_Arm, s_Claw));
-        eventMap.put("Balance", new Balance(s_Swerve));
+        // eventMap.put("event", new StowPosition(s_Elevator, s_Arm, s_Claw));
+        // eventMap.put("PickUp", new IntakeFromGroundPosition(s_Elevator, s_Arm, s_Claw));
+        // eventMap.put("PickupStow", new StowPosition(s_Elevator, s_Arm, s_Claw));
+        // eventMap.put("stopEvent", new Balance(s_Swerve));
+        // eventMap.put("Place", new MidNodePosition(s_Elevator, s_Arm, s_Claw));
+        // eventMap.put("PlaceStow", new StowPosition(s_Elevator, s_Arm, s_Claw));
+        // eventMap.put("Balance", new Balance(s_Swerve));
     }
 
     /** Use this method to add Autonomous paths, displayed with {@link AutonManager} */
@@ -165,9 +165,14 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return autonManager.getSelected();
-
-        // return new PathPlannerAuto(
+        // return autonManager.getSelected();
+        return autoBuilder.fullAuto(
+                PathPlanner.loadPathGroup(
+                        "RightPlacePickupPlaceBalance",
+                        new PathConstraints(
+                                Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+                                Constants.AutoConstants
+                                        .kMaxAccelerationMetersPerSecondSquared))); // return new PathPlannerAuto(
         //         PathPlanner.loadPath("Straight", new PathConstraints(6, 4)), s_Swerve, eventMap);
     }
 }
