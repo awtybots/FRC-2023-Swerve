47fd34c7d5973962ff420d26d920777f2f21bbd9
Terence Dumas <dumast@awty.org>
2023-03-13 12:04:15 -0500

    Led animations

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 
 public class LedSubsystem extends SubsystemBase {
 
@@ -10,8 +11,19 @@ public class LedSubsystem extends SubsystemBase {
     AddressableLEDBuffer m_ledBuffer;
     int length;
 
+    public static double ledCount = 0;
+    public static final double LED_SPEED = 1;
+    public final int stripLength;
+
+    public int[] rgb = new int[3];
+    
+    private int[] GREEN_CODE = {0, 255, 0};
+    private int[] PURPLE_CODE = {255, 0, 255};
+    private int[] YELLOW_CODE = {255, 255, 0};
+
     public LedSubsystem(int LEDPort, int length) {
         this.length = length;
+        this.stripLength = (int)(length / 2);
         m_led = new AddressableLED(LEDPort);
         m_ledBuffer = new AddressableLEDBuffer(length);
         m_led.setLength(m_ledBuffer.getLength());
@@ -19,12 +31,27 @@ public class LedSubsystem extends SubsystemBase {
         m_led.start();
     }
 
-    public void turnOn(int r, int g, int b) {
-        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-            m_ledBuffer.setRGB(i, r, g, b);
+    public void fillRange(int first, int last, int[] color){
+        for (int i = first; i < m_ledBuffer.getLength() && i < last; i++) {
+            m_ledBuffer.setRGB(i, color[0], color[1], color[2]);
         }
-        m_led.setData(m_ledBuffer);
     }
+
+    public void setColor(int c) {
+        if(c == 0){
+            rgb = PURPLE_CODE;
+        }
+        if(c == 1){
+            rgb = YELLOW_CODE;
+        }
+    }
+
+    public void setLed(int i, int[] color) {
+        if(i < 120 && i >=0){
+            m_ledBuffer.setRGB(i, color[0], color[1], color[2]);
+        }
+    }
+
 
     /** Method to turn on a certain percentage of the lights of a LED strip to a certain color. */
     public void turnOn(double portionLED, int r, int g, int b, int r2, int g2, int b2) {
@@ -56,14 +83,33 @@ public class LedSubsystem extends SubsystemBase {
         m_led.setData(m_ledBuffer);
     }
 
-    public void ledPatterns() {
-        for (var i = 0; i < m_ledBuffer.getLength(); i++) {}
-    }
-
     public void turnOff() {
         for (var i = 0; i < m_ledBuffer.getLength(); i++) {
             m_ledBuffer.setRGB(i, 0, 0, 0);
         }
         m_led.setData(m_ledBuffer);
+    }
+
+    @Override
+    public void periodic() {        
+        for(int i = 0; i < m_ledBuffer.getLength(); i++) {
+            setLed(i, GREEN_CODE);
+        }
+
+        if(RobotContainer.getIsCone())
+            setColor(1);
+        else
+            setColor(0);
+        ledCount += LED_SPEED;
+        for(int i = 0; i < stripLength; i++){
+            setLed(((int)(i + ledCount) % length), rgb);
+        }
+
+        if(ledCount > 120){
+            ledCount = 0;
+        }
+
+        m_led.setData(m_ledBuffer);
+        m_led.start();
     }
 }
