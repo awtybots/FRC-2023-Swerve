8b3504ff7ec0e726b59d1f81cf1ddc3d7799a9f5
Terence Dumas <dumast@awty.org>
2023-03-14 12:15:10 -0500

    changed robot width for pathplanner........

--- src/main/java/frc/robot/auto/Diagnostic.java
+++ src/main/java/frc/robot/auto/Diagnostic.java
@@ -2,7 +2,6 @@
 package frc.robot.auto;
 
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.commands.Positions.StowPosition;
 import frc.robot.subsystems.MechanicalParts.ArmSubsystem;
--- src/main/java/frc/robot/commands/Positions/Intake/IntakeFromGroundLowPosition.java
+++ src/main/java/frc/robot/commands/Positions/Intake/IntakeFromGroundLowPosition.java
@@ -2,7 +2,6 @@ package frc.robot.commands.Positions.Intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.MechanicalParts.ArmSubsystem;
 import frc.robot.subsystems.MechanicalParts.ClawSubsystem;
 import frc.robot.subsystems.MechanicalParts.ElevatorSubsystem;
